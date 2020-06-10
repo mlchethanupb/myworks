e493a65973b580488c6c6d6a37c3c9b77f52ce9e
stwerner97 <stefanwerner1997@gmail.com>
2020-06-10 16:10:52 +0200

    added state and action space representation

--- assignments/assignment_2/stwerner/flowsim/environment/packet.py
+++ assignments/assignment_2/stwerner/flowsim/environment/packet.py
@@ -3,15 +3,14 @@ import random
 
 
 class Packet(object):
-    def __init__(self, packet_size, start_time):     
+    def __init__(self, start_time):     
         # track packet information
-        self.id = random.randint(1, 1000)
-        self.packet_size = packet_size  # unit?
+        self.id = random.randint(1, 999999)
         self.start_time = start_time
-        self.hops = 0
+        self.hopcount = 0
 
     def update_statistics(self):
-        self.hops += 1
+        self.hopcount += 1
 
     def __repr__(self):
         return str(self.id)
