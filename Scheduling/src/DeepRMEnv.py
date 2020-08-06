3703121430cbababc813297c976edcbb55939bdf
AluriJaganMohini <mohini.spr2019@gmail.com>
2020-08-06 19:00:25 +0200

    Training the agent using stable baselines

--- Scheduling/src/DeepRMEnv.py
+++ Scheduling/src/DeepRMEnv.py
@@ -2,155 +2,171 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import parameters
+import argparse
+from random import sample
+import os
+from networkx import nx
+import gym
+from stable_baselines.deepq.policies import MlpPolicy
+from stable_baselines import A2C
+from stable_baselines import PPO2
+from stable_baselines.common.env_checker import check_env
+from gym import spaces
 
 
-class Env :
-	def __init__(self , pa , job_sequence_len = None , job_sequence_size = None , end = 'no_new_job') :
-		
-		self.pa = pa
-		self.end = end  # termination type, 'no_new_job' or 'all_done'
-		
-		self.curr_time = 0
-		self.job_sequence_len = job_sequence_len
-		self.job_sequence_size = job_sequence_size
-		self.seq_no = 0  # which example sequence
-		self.seq_idx = 0  # index in that sequence
-		
-		# initialize system
-		self.machine = Machine(pa)
-		self.job_slot = JobSlot(pa)
-		self.job_backlog = JobBacklog(pa)
-		self.job_record = JobRecord()
+class Env(gym.Env):
+    def __init__(self , pa , job_sequence_len = None , job_sequence_size = None , end = 'no_new_job') :
+        super(Env, self).__init__()
+        self.pa = pa
+        self.end = end  # termination type, 'no_new_job' or 'all_done'
+        self.curr_time = 0
+        self.job_sequence_len = job_sequence_len
+        self.job_sequence_size = job_sequence_size
+        self.seq_no = 0  # which example sequence
+        self.seq_idx = 0  # index in that sequence
+        
+        # initialize system
+        self.machine = Machine(pa)
+        self.job_slot = JobSlot(pa)
+        self.job_backlog = JobBacklog(pa)
+        self.job_record = JobRecord()
+        state_space = self.observe()
+    
+        action_space = 6 #[i for i in range(pa.job_wait_queue)]
+        #action_space.append(9999)
+        self.state_space = state_space
+        self.action_space = spaces.Discrete(action_space)
+        self.observation_space = spaces.Box(low=0, high=6, shape=(20,2), dtype=np.int64)
 	
-	def step(self , a) :
-		status = None
-		done = False
-		reward = 0
-		info = None
+    def step(self , a) :
+	    status = None
+	    done = False
+	    reward = 0
+	    info = None
 		
-		if a == self.pa.job_wait_queue :  # explicit void action
-			status = 'MoveOn'
-		elif self.job_slot.slot[a] is None :  # implicit void action
-			status = 'MoveOn'
-		else :
-			allocated = self.machine.allocate_job(self.job_slot.slot[a] , self.curr_time)
-			if not allocated :  # implicit void action
-				status = 'MoveOn'
-			else :
-				status = 'Allocate'
+	    if a == self.pa.job_wait_queue :  # explicit void action
+		    status = 'MoveOn'
+	    elif self.job_slot.slot[a] is None :  # implicit void action
+		    status = 'MoveOn'
+	    else :
+		    allocated = self.machine.allocate_job(self.job_slot.slot[a] , self.curr_time)
+		    if not allocated :  # implicit void action
+			    status = 'MoveOn'
+		    else :
+			    status = 'Allocate'
 		
-		if status == 'MoveOn' :
-			self.curr_time += 1
-			self.machine.time_proceed(self.curr_time)
+	    if status == 'MoveOn' :
+		    self.curr_time += 1
+		    self.machine.time_proceed(self.curr_time)
 			
-			if self.end == "no_new_job" :  # end of new job sequence
-				if self.seq_idx >= self.pa.simu_len :
-					done = True
-			elif self.end == "all_done" :
+		    if self.end == "no_new_job" :  # end of new job sequence
+			    if self.seq_idx >= self.pa.simu_len :
+				    done = True
+		    elif self.end == "all_done" :
 				# Done is true if no jobs are running in th machine + no job in the waiting queue and
 				# no jobs in the backlog
-				if self.seq_idx >= self.pa.simu_len and len(self.machine.running_job) == 0 and \
+			    if self.seq_idx >= self.pa.simu_len and len(self.machine.running_job) == 0 and \
 						all(s is None for s in self.job_slot.slot) and \
 						all(s is None for s in self.job_backlog.backlog) :
-					done = True
+				    done = True
 				# Deadlock handling
-				elif self.curr_time > self.pa.episode_max_length :  # run too long, force termination
-					done = True
+			    elif self.curr_time > self.pa.episode_max_length :  # run too long, force termination
+				    done = True
 			
-			if not done :
-				if self.seq_idx < self.pa.simu_len :  # otherwise, end of new job sequence, i.e. no new jobs
-					new_job = self.get_new_job_from_seq(self.seq_no , self.seq_idx)
-					self.seq_idx += 1
-					if new_job.len > 0 :
-						add_to_backlog = True
+		    if not done :
+			    if self.seq_idx < self.pa.simu_len :  # otherwise, end of new job sequence, i.e. no new jobs
+				    new_job = self.get_new_job_from_seq(self.seq_no , self.seq_idx)
+				    self.seq_idx += 1
+				    if new_job.len > 0 :
+					    add_to_backlog = True
 						# If the job slot is empty then only put the new job in the job slot/waiting queue
 						# If the job slot is not empty then the job will go in the job backlog
-						for i in range(self.pa.job_wait_queue) :
-							if self.job_slot.slot[i] is None :  # put in new visible job slots
-								self.job_slot.slot[i] = new_job
-								self.job_record.record[new_job.id] = new_job
-								add_to_backlog = False
-								break
-						
-						if add_to_backlog :
-							if self.job_backlog.curr_size < self.pa.backlog_size :
-								self.job_backlog.backlog[self.job_backlog.curr_size] = new_job
-								self.job_backlog.curr_size += 1
-								self.job_record.record[new_job.id] = new_job
-							else :  # abort, backlog full
-								print("Backlog full.")
-			reward = self.get_reward()
+					    for i in range(self.pa.job_wait_queue) :
+						    if self.job_slot.slot[i] is None :  # put in new visible job slots
+							    self.job_slot.slot[i] = new_job
+							    self.job_record.record[new_job.id] = new_job
+							    add_to_backlog = False
+							    break
+                                
+					    if add_to_backlog :
+						    if self.job_backlog.curr_size < self.pa.backlog_size :
+							    self.job_backlog.backlog[self.job_backlog.curr_size] = new_job
+							    self.job_backlog.curr_size += 1
+							    self.job_record.record[new_job.id] = new_job
+						    else :  # abort, backlog full
+							    print("Backlog full.")
+		    reward = self.get_reward()
 		
-		elif status == 'Allocate' :
-			self.job_record.record[self.job_slot.slot[a].id] = self.job_slot.slot[a]
-			self.job_slot.slot[a] = None
+	    elif status == 'Allocate' :
+		    self.job_record.record[self.job_slot.slot[a].id] = self.job_slot.slot[a]
+		    self.job_slot.slot[a] = None
 			
 			# dequeue backlog
-			if self.job_backlog.curr_size > 0 :
-				self.job_slot.slot[a] = self.job_backlog.backlog[0]  # if backlog empty, it will be 0
-				self.job_backlog.backlog[: -1] = self.job_backlog.backlog[1 :]
-				self.job_backlog.backlog[-1] = None
-				self.job_backlog.curr_size -= 1
+		    if self.job_backlog.curr_size > 0 :
+			    self.job_slot.slot[a] = self.job_backlog.backlog[0]  # if backlog empty, it will be 0
+			    self.job_backlog.backlog[: -1] = self.job_backlog.backlog[1 :]
+			    self.job_backlog.backlog[-1] = None
+			    self.job_backlog.curr_size -= 1
 		
-		ob = self.observe()
-		if done :
-			self.seq_idx = 0
-			self.seq_no = 0
-			self.reset()
-		return ob , reward , done , info
+	    ob = self.observe()
+	    if done :
+		    self.seq_idx = 0
+		    self.seq_no = 0
+		    self.reset()
+	    return ob , reward , done , info
 	
 	# Get new job from waiting queue
-	def get_new_job_from_seq(self , seq_no , seq_idx) :
-		new_job = Job(resorce_requirement = self.job_sequence_size[seq_idx] ,
+    def get_new_job_from_seq(self , seq_no , seq_idx) :
+	    new_job = Job(resorce_requirement = self.job_sequence_size[seq_idx] ,
 		              job_len = self.job_sequence_len[seq_idx] ,
 		              job_id = len(self.job_record.record) ,
 		              enter_time = self.curr_time)
-		return new_job
+	    return new_job
 	
-	def observe(self) :
-		ob = [[] , [] , []]
+    def observe(self) :
+	    ob = [[] , [] , []]
 
 		# Current allocation of cluster resources
-		ob[0] = list(self.machine.available_res_slot)
+	    ob[0] = list(self.machine.available_res_slot)
 		
 		# The resource profile of jobs in the job slot queue/waiting queue/M
-		for i in self.job_slot.slot :
+	    for i in self.job_slot.slot :
 			#                  [[length],[Resource requirement]]
-			if i is not None :
-				resource_profile = [i.len , i.resorce_requirement]
-				ob[1].append(resource_profile)
+		    if i is not None :
+			    resource_profile = [i.len , i.resorce_requirement]
+			    ob[1].append(resource_profile)
 		
 		# The resource profile of the jobs in the backlog
-		for i in self.job_backlog.backlog :
-			if i is not None :
-				resource_profile = [i.len , i.resorce_requirement]
-				ob[2].append(resource_profile)
+	    for i in self.job_backlog.backlog :
+		    if i is not None :
+			    resource_profile = [i.len , i.resorce_requirement]
+			    ob[2].append(resource_profile)
 		
-		return ob
+	    return ob
 	
-	def reset(self) :
-		self.seq_idx = 0
-		self.curr_time = 0
+    def reset(self) :
+	    self.seq_idx = 0
+	    self.curr_time = 0
 		# initialize system
-		self.machine = Machine(self.pa)
-		self.job_slot = JobSlot(self.pa)
-		self.job_backlog = JobBacklog(self.pa)
-		self.job_record = JobRecord()
+	    self.machine = Machine(self.pa)
+	    self.job_slot = JobSlot(self.pa)
+	    self.job_backlog = JobBacklog(self.pa)
+	    self.job_record = JobRecord()
 	
-	def get_reward(self) :
-		reward = 0
+    def get_reward(self) :
+	    reward = 0
 		# Reward based on jobs currently running
-		for j in self.machine.running_job :
-			reward += self.pa.penalty / float(j.len)
+	    for j in self.machine.running_job :
+		    reward += self.pa.penalty / float(j.len)
 		# Reward based on job in the waiting queue
-		for j in self.job_slot.slot :
-			if j is not None :
-				reward += self.pa.penalty / float(j.len)
+	    for j in self.job_slot.slot :
+		    if j is not None :
+			    reward += self.pa.penalty / float(j.len)
 		# Reward based on job in the backlog
-		for j in self.job_backlog.backlog :
-			if j is not None :
-				reward += self.pa.penalty / float(j.len)
-		return reward
+	    for j in self.job_backlog.backlog :
+		    if j is not None :
+			    reward += self.pa.penalty / float(j.len)
+	    return reward
 
 
 class Job :
