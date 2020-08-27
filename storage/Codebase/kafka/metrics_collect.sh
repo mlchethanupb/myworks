6a34e9503710358d0e3dc7f2ab03ebedf4e1f3e2
lameya116 <34994754+lameya116@users.noreply.github.com>
2020-08-27 13:17:03 +0200

    Update metrics_collect.sh

--- storage/Codebase/kafka/metrics_collect.sh
+++ storage/Codebase/kafka/metrics_collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #cd ~/kafka_2.12-2.2.0
-diskSpace=$(docker exec -t -i 49394bfbacd0 /bin/sh -c "gluster volume status vol_331392cba7ebacda46a545d968f8d9af detail | grep 'Disk Space Free' | cut -f2 -d":" | sed 's/^ *//g'| head -n1")
+diskSpace=$(docker exec -t -i 49394bfbacd0 /bin/sh -c "gluster volume status vol_331392cba7ebacda46a545d968f8d9af detail | grep 'Disk Space Free' | cut -f2 -d":" | head -n1 |sed 's/^ *//g'")
 echo "$diskSpace">>currentMetrics.txt
 consumedData=$(docker exec -t -i kafka bin/kafka-consumer-perf-test.sh --topic dt-imms-opcua-0  --broker-list localhost:9092 --messages 100 --threads 2 | sed -n 2p | cut -d, -f3)
 echo "$consumedData"| xargs>>currentMetrics.txt
@@ -8,7 +8,7 @@ nMsg=$(docker exec -t -i kafka bin/kafka-consumer-perf-test.sh --topic dt-imms-o
 echo "$nMsg"| xargs>>currentMetrics.txt
 avgLatency=$(docker exec -t -i kafka bin/kafka-producer-perf-test.sh --topic dt-imms-opcua-0 --num-records 50 --throughput 10 --producer-props bootstrap.servers=localhost:9092 --record-size 1 | cut -d, -f3|sed 's/[^0-9]*//g')
 echo "$avgLatency"| xargs>>currentMetrics.txt
-maxLatency=$(docker exec -t -i kafka bin/kafka-producer-perf-test.sh --topic dt-imms-opcua-0 --num-records 50 --throughput 10 --producer-props bootstrap.servers=localhost:9092 --record-size 1 | cut -d, -f4	)
+maxLatency=$(docker exec -t -i kafka bin/kafka-producer-perf-test.sh --topic dt-imms-opcua-0 --num-records 50 --throughput 10 --producer-props bootstrap.servers=localhost:9092 --record-size 1 | cut -d, -f4|sed 's/[^0-9]*//g')
 echo "$maxLatency"| xargs>>currentMetrics.txt
 inByte=$(docker exec -t -i kafka bin/kafka-producer-perf-test.sh --topic dt-imms-opcua-0 --num-records 50 --throughput 10 --producer-props bootstrap.servers=localhost:9092 --record-size 1 --print-metrics| grep 'incoming-byte-rate'|cut -f4 -d":"| sed -n 1p)
 echo "$inByte"| xargs>>currentMetrics.txt
@@ -17,7 +17,5 @@ echo "$outByte"| xargs>>currentMetrics.txt
 reqRate=$(docker exec -t -i kafka bin/kafka-producer-perf-test.sh --topic dt-imms-opcua-0 --num-records 50 --throughput 10 --producer-props bootstrap.servers=localhost:9092 --record-size 1 --print-metrics| grep 'request-rate' | cut -f4 -d":"| sed -n 1p)
 echo "$reqRate"| xargs>>currentMetrics.txt
 #sed 's/\s\+/,/g' a.csv>>tt.txt 
-
-
 
 
