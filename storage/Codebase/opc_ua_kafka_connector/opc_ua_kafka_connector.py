6e3860810ae77b6155c1025c17a4054d6d74f140
lameya116 <34994754+lameya116@users.noreply.github.com>
2020-09-13 12:33:27 +0200

    Update opc_ua_kafka_connector.py

--- storage/Codebase/opc_ua_kafka_connector/opc_ua_kafka_connector.py
+++ storage/Codebase/opc_ua_kafka_connector/opc_ua_kafka_connector.py
@@ -11,6 +11,12 @@ import threading
 from queue import Queue
 import _thread
 
+import csv
+lines = []
+print("program started")
+from subprocess import call
+tempList=[]
+
 INSTANCE_NAME = os.environ.get('INSTANCE_NAME', 'MY-INSTACE')
 OPCUA_SERVER = os.environ.get('OPCUA_SERVER', 'localhost')
 OPCUA_PORT = os.environ.get('OPCUA_PORT', '4840')
@@ -20,17 +26,20 @@ KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC', 'my-topic')
 SLEEP_DURATION = os.environ.get('SLEEP_DURATION', '1') # seconds sleep after each update written to kafka
 OUT_FILE = os.environ.get('OUT_FILE', 'response_time.json')
 NUMBER_MESSAGES = os.environ.get('NUMBER_MESSAGES', 10)
-
+t_slot=4
 number_messages = 0
+set_c=0
 q = Queue()
 file = open(OUT_FILE, "w")
-
+msg_arr=[]
 kafka_consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers='%s:%s' % (KAFKA_SERVER, KAFKA_PORT),
                              # auto_offset_reset='earliest',
                              consumer_timeout_ms=30000)
 
 def _monitor_response_time(kafka_consumer, q):
     global number_messages
+    t1 = datetime.now()
+
     while True:
         try:
             message = next(kafka_consumer)
@@ -38,12 +47,25 @@ def _monitor_response_time(kafka_consumer, q):
             msg = json.loads(message.value.decode('utf-8'))
             timestamp_sent = datetime.strptime(str(msg['TimestampSent']), '%Y-%m-%d %H:%M:%S.%f')
             response_time = timestamp_received - timestamp_sent
-            print("Response time: %f seconds" % response_time.total_seconds())
+            #print("Response time: %f seconds" % response_time.total_seconds())
+            #print("\n\n\nSlot time",s_time)
             q.put(response_time.total_seconds())
+            tempList.append(response_time.total_seconds())
             number_messages += 1
+            if s_time==0 or s_time % t_slot==1:
+                t11 = datetime.now()
+            elif s_time% t_slot==0 :
+                t22 = datetime.now()
+                t33=t22-t11
+                print("\nCon time: %f seconds" % t33.total_seconds())
+
+
             if int(NUMBER_MESSAGES) > 0:
                 if number_messages >= int(NUMBER_MESSAGES):
                     _thread.interrupt_main()
+                    t2 = datetime.now()
+                    t3=t2-t1
+                    #print("consume time: %f seconds" % t3.total_seconds())                   
                     sys.exit()
         except StopIteration:
             pass
@@ -73,9 +95,13 @@ if __name__== "__main__":
     kafka_producer.flush()
 
     print('Connected to Kafka %s:%s' % (KAFKA_SERVER, KAFKA_PORT))
+    s_time=0
+    if s_time==0:
+        t4 = datetime.now()
 
     while True:
         try:
+            s_time=s_time+1
             msg = {
                 "date": float(imms.get_variables()[0].get_value()),
                 "time": str(imms.get_variables()[1].get_value()),
@@ -91,10 +117,43 @@ if __name__== "__main__":
                 "InstanceName": INSTANCE_NAME,
                 "TimestampSent": datetime.now()
             }
+            
 
             future = kafka_producer.send(KAFKA_TOPIC, json.dumps(msg, default=str).encode('utf-8'))
+            #msg1=msg.encode("utf-8")
+            msg_size=int(sys.getsizeof(msg))
+            msg_arr.append(msg_size)
             kafka_producer.flush()
-            print('Update written to topic %s' % KAFKA_TOPIC)
+            if s_time % t_slot==1:
+                t4 = datetime.now()
+
+            elif s_time% t_slot==0 and s_time>0:
+                t5 = datetime.now()
+
+                t6=t5-t4
+                print("\nPro time: %f seconds" % t6.total_seconds())
+                arr_s=s_time-t_slot
+                arr_e=s_time+t_slot-1
+                set_c=set_c+1
+                #print("\n\n Set val:",set_c)
+                chk_arr=[]
+                chk_msg=[]
+                chk_arr=tempList[arr_s:arr_e]
+                chk_msg=msg_arr[arr_s:arr_e]
+                maxList=max(chk_arr)
+                sumList=sum(chk_arr)
+                sumMsg=sum(chk_msg)
+                avgList=float(sumList/int(t_slot))
+                print("\n\nMax List",maxList,"\nWhole List",tempList,"\nAverage of List",avgList,"\nMsg size",sumMsg)
+                
+
+
+                
+
+            #print('Update written to topic %s' % KAFKA_TOPIC)
+            #with open('/home/user/dt/met.sh', 'rb') as file:
+                #script = file.read()
+            #rc = call(script, shell=True)            
 
             time.sleep(float(SLEEP_DURATION))
         except KeyboardInterrupt:
