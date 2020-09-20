78637b99e1b015c27f97e20955e77ab46ec561cf
lameya116 <34994754+lameya116@users.noreply.github.com>
2020-09-20 13:56:41 +0200

    Delete met.sh

--- storage/Codebase/opc_ua_kafka_connector/met.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-diskSpace=$(docker exec -t -i 49394bfbacd0 /bin/sh -c "gluster volume status vol_331392cba7ebacda46a545d968f8d9af detail | grep 'Disk Space Free' | cut -f2 -d":" | head -n1 |sed 's/^ *//g'")
-echo "$diskSpace">>c.txt
