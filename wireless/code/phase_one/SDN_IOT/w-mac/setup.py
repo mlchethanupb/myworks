6bf50dafd99cdc156e8494dcc76c62503a20b898
Chethan M L <mlchethan0105@gmail.com>
2020-10-20 02:11:26 +0200

    basic tuning working code

--- wireless/code/phase_one/SDN_IOT/w-mac/setup.py
+++ wireless/code/phase_one/SDN_IOT/w-mac/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='w_mac',
-      version='0.0.1',
+      version='SDN-IOT',
       install_requires=['gym','networkx']#And any other dependencies required
 )
--- wireless/code/phase_one/Tuning/w-mac/setup.py
+++ wireless/code/phase_one/Tuning/w-mac/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='w_mac',
-      version='0.0.2',
+        version='tuning-01',
       install_requires=['gym','networkx']#And any other dependencies required
 )
