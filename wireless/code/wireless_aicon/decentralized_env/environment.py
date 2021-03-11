138ce8acdaaf417d02118dcde9cc4df36f026c77
Chethan M L <mlchethan0105@gmail.com>
2021-03-11 11:20:17 +0100

    update on scaling script

--- wireless/code/wireless_aicon/decentralized_env/environment.py
+++ wireless/code/wireless_aicon/decentralized_env/environment.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 
 from gym.spaces import Discrete, MultiDiscrete, Tuple, Box
-from packet import Packet
+from decentralized_env.packet import Packet
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
 class WirelessEnv(MultiAgentEnv):
