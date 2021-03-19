d53dc5bbb52d766b0450fa6cce5295015dcc68da
Chethan M L <mlchethan0105@gmail.com>
2021-03-19 09:12:05 +0100

    Graph update

--- wireless/code/wireless_aicon/scaling.py
+++ wireless/code/wireless_aicon/scaling.py
@@ -20,17 +20,18 @@ parser.add_argument("--run", type=str, default="PPO")
 parser.add_argument("--torch", action="store_true")
 parser.add_argument("--stop_timesteps", type=int, default=500000)
 parser.add_argument('--graph', type=str, nargs='?', const=1, default='[(0, 2), (0, 1), (0, 3), (1, 2), (1, 3), (2, 3),(2, 4), (3, 4), (5, 2), (5, 3), (5, 4)]', help='Pass a networkx graph or \'default\'')
-
+#[ (0,1), (0,2), (0,3), (1,2), (1,3), (2,3), (2,4), (3,4), (5,2), (5,3), (5,4)],
+"""
+    [ (0,1), (0,2), (0,3), (0,4), (1,2), (1,3), (1,4), (2,3), (2,4), 
+      (3,4), (3,5), (3,6), (3,7), (4,5), (4,6), (4,7), 
+      (5,6), (5,7), (6,7) ],
+"""
 all_graphs = [ 
-    [ (0,1), (0,2), (0,3), (1,2), (1,3), (2,3), (2,4), (3,4), (5,2), (5,3), (5,4)],
-    [ (0,1), (0,2), (0,3), (0,4), (0,5), (1,2), (1,3), (1,4), (1,5), (2,3), (2,4), 
-      (2,5), (3,4), (3,5), (3,6), (3,7), (3,8), (3,9), (4,5), (4,6), (4,7), (4,8), 
-      (4,9), (5,6), (5,7), (5,8), (5,9), (6,7), (6,8), (6,9), (7,8), (7,9), (8,9)],
-    [  (0,1), (0,2), (0,3), (0,4), (0,5), (0,6), (0,7), (1,2), (1,3), (1,4), (1,5),
-       (1,6), (1,7), (2,3), (2,4), (2,5), (2,6), (2,7), (3,4), (3,5), (3,6), (3,7),
-       (4,5), (4,6), (4,7), (5,6), (5,7), (5,8), (5,9), (5,10),(5,11),(5,12),(6,7),
-       (6,8), (6,9), (6,10),(6,11),(6,12),(7,8), (7,9), (7,10),(7,11),(7,12),(8,9),
-       (8,10),(8,11),(8,12),(9,10),(9,11),(9,12),(10,11),(10,12),(11,12) ]
+
+    [  (0,1), (0,2), (0,3), (0,4), (0,5), (1,2), (1,3), (1,4), (1,5),
+       (2,3), (2,4), (2,5), (3,4), (3,5), (3,6), (3,7), (3,8),
+       (4,5), (4,6), (4,7), (4,8), (5,6), (5,7), (5,8), (6,7),
+       (6,8), (7,8) ]
 ]
 
 def create_graph(graph_list):
@@ -62,7 +63,6 @@ def get_centralized_config(graph: nx.Graph):
                 "num_cpus_per_worker": 2,
                 "num_gpus": 0,
                 "num_envs_per_worker": 1,
-                "lr": 0.001,
                 "env": "centalized_env",
                 "framework": "torch" if args.torch else "tf"
     }
@@ -101,7 +101,6 @@ def get_decentralized_config(graph: nx.Graph):
                 "num_gpus": 0,
                 "num_envs_per_worker": 1,
                 "no_done_at_end": True,
-                "lr": 0.001,
                 "multiagent": {
                     "policies": policy_graphs,
                     "policy_mapping_fn": policy_mapping_fn,
@@ -118,7 +117,7 @@ def get_exp_dict(config, centralized_flag):
 
         total_timesteps = args.stop_timesteps 
         if centralized_flag == False:
-            total_timesteps = args.stop_timesteps * 1.5
+            total_timesteps = args.stop_timesteps
         
         exp_dict = {
             'name': exp_name,
@@ -149,7 +148,7 @@ if __name__ == "__main__":
     for list_itr in all_graphs:
         graph = create_graph(list_itr)
     
-    """
+
     for list_itr in all_graphs:
         ray.init()
         graph = create_graph(list_itr)
@@ -157,7 +156,7 @@ if __name__ == "__main__":
         exp_dict = get_exp_dict(config, True)
         train_model(exp_dict)
         ray.shutdown()
-    
+    """
     for list_itr in all_graphs:
         ray.init()
         graph = create_graph(list_itr)
